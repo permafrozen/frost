20b27f4ba5f0af9d16314ffad010e4b95bfdf4e3
Permafrozen <m8tt8o@gmail.com>
2025-02-25 11:23:55 +0100

    fixed production path in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,14 @@ import adapter from "@sveltejs/adapter-static";
 export default {
     kit: {
         adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
             pages: "build",
             assets: "build",
-            fallback: undefined,
             precompress: false,
             strict: true,
+            fallback: "404.html",
         }),
         paths: {
-            base: process.env.NODE_ENV === "production" ? "/{frost}" : "",
+            base: process.env.NODE_ENV === "production" ? "/frost" : "",
         },
     },
 };
