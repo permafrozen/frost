2f0731b156ccd026faa7f3224d2f23184677e1d8
Permafrozen <m8tt8o@gmail.com>
2025-04-13 17:39:04 +0200

    added tailwindcss and simple usage

---
+++ src/lib/components/heading1.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let label = "";
+</script>
+
+<h1 class="text-4xl font-bold">{label}</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import H1 from "$lib/components/heading1.svelte";
+</script>
+
+<div class="flex flex-col justify-start p-20 items-start h-screen *:pb-25">
+    <div>
+        <H1 label="HELLO WORLD"></H1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum voluptas amet
+        dolorem, sequi aut hic quasi eos explicabo repudiandae, accusantium corporis
+        quia velit perferendis ab veniam, nemo asperiores? Maiores, odio.
+    </div>
+    <div>
+        <H1 label="About" />
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime velit ab
+        alias ut dolor tempora optio quia consequuntur obcaecati consectetur, saepe
+        itaque dolorum placeat repellat dolorem exercitationem, deserunt quam animi?
+    </div>
+    <div>
+        <H1 label="Interests"></H1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aperiam placeat
+        nobis excepturi qui facilis soluta laborum pariatur dolorum magnam exercitationem,
+        dolor possimus. Suscipit culpa laboriosam cum, reiciendis voluptates nam?
+    </div>
+    <div>
+        <H1 label="Projects"></H1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam
+        aperiam, quaerat deserunt ducimus officia repellendus doloribus nobis. Reprehenderit
+        ut ipsa consequatur illo beatae eaque non maiores magni eveniet fuga?
+    </div>
+</div>
